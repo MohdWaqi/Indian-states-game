20d62ffdec5f0a67cac52172fa23bab7ee19a987
Mohd Waqi Pervez <mohdwaqipervez@gmail.com>
2023-04-19 17:27:24 +0530

    Added comments for better understanding

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import turtle
 import pandas
+
+########################### Setting up the screen ##################################
+
 screen = turtle.Screen()
 screen.setup(width=650, height=750)
 screen.addshape("Picture1 - Copy.gif")
@@ -10,20 +13,28 @@ image = turtle.shape("Picture1 - Copy.gif")
 #
 # screen.onscreenclick(click)
 # screen.mainloop()
+
+##################################### Reading the data from states.csv #########################
+
 data = pandas.read_csv("states.csv")
 all_states = data.states.to_list()
-guessed_states =0
-guessed_list =[]
+guessed_states = 0
+guessed_list = []
 should_continue = True
+
 while should_continue:
-    user = (screen.textinput(title=f"{guessed_states}/36 Guess the state", prompt="What's the name of the state?")).title()
+    user = (
+        screen.textinput(title=f"{guessed_states}/36 Guess the state", prompt="What's the name of the state?")).title()
     if user in all_states:
         guessed_states += 1
         state = turtle.Turtle()
         state.hideturtle()
         state.penup()
+
+        ###################### showing the state name on map if guessed correctly #####################
+
         position = data[data.states == user]
-        state.goto(int(position.x), int(position.y))
+        state.setposition(int(position.x), int(position.y))
         state.write(arg=f"{user}")
         guessed_list.append(user)
     if user == "Off" or guessed_states == 36:
@@ -31,4 +42,5 @@ while should_continue:
 not_guessed = [state for state in all_states if state not in guessed_list]
 learn = pandas.DataFrame(not_guessed)
 learn.to_csv("states_to_learn")
+
 screen.exitonclick()
